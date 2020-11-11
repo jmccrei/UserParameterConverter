4492440120ff6513e7e03348056cf273abfc0e2c
Josh McCreight <josh.mccreight@checkfront.com>
2020-11-10 16:50:49 -0800

    Update namespace

--- src/Request/UserParamConverter.php
+++ src/Request/UserParamConverter.php
@@ -6,7 +6,7 @@
 
 declare(strict_types=1);
 
-namespace App\Request;
+namespace Jmccrei\UserParameterConverter\Request;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
